24fbbafccebb8c31e6ba36f8d3e0af7d1a07ff1a
I AM MIKEY <86167689+MoeZilla@users.noreply.github.com>
2021-11-16 13:58:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000
 const UserRouter = require('./routers/user');
 
+const cors = require('cors')
+const NodeCache = require('node-cache')
 
 const host = process.env.host
 const db = process.env.db
