b5c4c4c60d523e4b04b978134d2309eabd216c17
I AM MIKEY <86167689+MoeZilla@users.noreply.github.com>
2021-11-16 15:20:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const moe = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000
-const User = require('./models/User');
 
 const cors = require('cors')
 const NodeCache = require('node-cache')
@@ -16,7 +15,7 @@ moe.use(cors())
 moe.use(express.json())
 moe.use(express.urlencoded({ extended: false }))
 
-moe.get("/", (req, res) => {
+moe.get("/home", (req, res) => {
     res.status(200).json({
         "api": "getinfo/{userid}",
 
@@ -25,16 +24,10 @@ moe.get("/", (req, res) => {
 
 moe.get("/getinfo/:id", async(req, res) =>{
             const id = request.params.id
-         try {
-            const User = await sylviorus.findOne({ user: id })
+            const user = await sylviorus.findOne({ user: id })
             res
             .status(200)
-            .json(User);
-        } catch (err) {
-            res
-            .status(500)
-            .json(err);
-        }
+            .json(user);
 });
 
 moe.listen( port, () => {
