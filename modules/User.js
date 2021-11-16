a9da9c78b1268544790ae594c482f8489e605087
I AM MIKEY <86167689+MoeZilla@users.noreply.github.com>
2021-11-16 08:02:00 +0530

    Update User.js

--- modules/User.js
+++ modules/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  userid: {
+  user: {
     type: Number,
     unique: true,
     required: true
